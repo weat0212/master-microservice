913a05581e57a0f0632c84e7ddaa8875573f1957
WEBCOMM\andy.wang <weat0212@gmail.com>
2021-08-23 17:10:11 +0800

    jpa configuration

--- currency-exchange-service/src/main/java/com/example/currencyexchangeservice/ExchangeValue.java
+++ currency-exchange-service/src/main/java/com/example/currencyexchangeservice/ExchangeValue.java
@@ -5,13 +5,17 @@ import java.math.BigDecimal;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity
+@Table(name = "exchange_value")
 public class ExchangeValue {
 
 	@Id
@@ -23,7 +27,10 @@ public class ExchangeValue {
 	@Column(name="currency_to")
 	private String to;
 	
+	@Column(name="conversion_multiple")
 	private BigDecimal conversionMultiple;
+	
+	@Column(name="port")
 	private int port;
 	
 }
