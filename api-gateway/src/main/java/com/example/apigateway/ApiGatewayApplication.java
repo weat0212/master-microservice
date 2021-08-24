3c23f303f16ecb62ce1622e23d588874ba4d35c4
WEBCOMM\andy.wang <weat0212@gmail.com>
2021-08-24 16:17:13 +0800

    init api gateway

--- api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -1,13 +1,13 @@
-package com.example.netflixapigatewayserver;
+package com.example.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NetflixApiGatewayServerApplication {
+public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NetflixApiGatewayServerApplication.class, args);
+		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
 }
