410833851c908565c279151e54cf3bd6dae15a55
WEBCOMM\andy.wang <weat0212@gmail.com>
2021-08-24 14:58:10 +0800

    connect microservice to Eureka

--- currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,12 +2,14 @@ package com.example.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients("com.example.currencyconversionservice")
+@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
