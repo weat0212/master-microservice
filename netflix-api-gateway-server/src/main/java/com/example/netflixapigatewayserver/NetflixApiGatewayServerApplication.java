71971107661c4091dacd9992e337797d92dc582e
WEBCOMM\andy.wang <weat0212@gmail.com>
2021-08-24 15:57:19 +0800

    init api gateway

---
+++ netflix-api-gateway-server/src/main/java/com/example/netflixapigatewayserver/NetflixApiGatewayServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.netflixapigatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixApiGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixApiGatewayServerApplication.class, args);
+	}
+
+}
---
+++ netflix-api-gateway-server/src/test/java/com/example/netflixapigatewayserver/NetflixApiGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.netflixapigatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixApiGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
